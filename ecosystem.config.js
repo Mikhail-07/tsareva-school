1a18881eff693c29b83a33d234aa6f0f527e530c
Mikhail Lagutik <lma3588329@gmail.com>
2024-11-07 19:56:36 +0300

    upd

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: "next-app",     // Название приложения
+      script: "node_modules/next/dist/bin/next",  // Путь к бинарнику Next.js
+      args: "start -p 3000", // Аргументы для запуска в режиме production на порту 3000
+      instances: "max",      // Максимальное количество инстансов, зависит от количества ядер CPU
+      exec_mode: "cluster",  // Режим кластера для обработки запросов параллельно
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
